62b7f707424461ed31409496de26a82841419a82
CryptoCanary <ssaxton@kent.edu>
2018-11-07 23:50:52 -0500

    Rebranding to Sterlingcoin

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19009 : 9009;
+    return testnet ? 19127 : 9127;
 }
 
 //
